ef130de90b1649d35dd0849ccd2ad2193068effe
Danas <sutula.danas@gmail.com>
2020-03-09 13:55:13 +0200

    Solve time-dependent wave equation using theta-method.

--- main.py
+++ main.py
@@ -4,60 +4,135 @@ from dolfin import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-nx = ny = 240
+nx = ny = 60
 diagonal = "crossed"
-degree = 2
+degree = 1
 mesh = UnitSquareMesh(nx, ny, diagonal=diagonal)
+
+outfile_u = File("./out/u.pvd")
+
+### Parameters
+
+c = 1.0
+omega = 2*np.pi
+
+nk = 2400
+t_final = 8
+dt = t_final / nk
+
+theta = 1.0
+
+c_ = Constant(c)
+dt_ = Constant(dt)
+theta_ = Constant(theta)
+
+
 
 ### Source term
 
-g1 = Expression(" exp(-50*(pow(x[0], 2) + pow(x[1], 2)))", degree=3)
-g2 = Expression(" exp(-50*(pow((x[0]-1.0), 2) + pow((x[1]-1.0), 2)))", degree=3)
-g3 = Expression(" exp(-50*(pow((x[0]-0.5), 2) + pow((x[1]-0.5), 2)))", degree=4)
+# g1 = Expression(" exp(-50*(pow(x[0], 2) + pow(x[1], 2)))", degree=3)
+# g2 = Expression(" exp(-50*(pow((x[0]-1.0), 2) + pow((x[1]-1.0), 2)))", degree=3)
+# g3 = Expression(" exp(-50*(pow((x[0]-0.5), 2) + pow((x[1]-0.5), 2)))", degree=4)
 
-g = g3
+# gn = Constant(0.0)
+# N = FacetNormal(mesh)
+# g = gn * N
+
+### Boundary conditions
+
+u_D = Expression((f"sin({omega}*t)", "0.0"), t=0.0, degree=3)
+dudt_D = Expression((f"{omega}*cos({omega}*t)", "0.0"), t=0.0, degree=3)
 
 ### Function space
 
 V = VectorFunctionSpace(mesh, 'CG', degree)
 S = FunctionSpace(mesh, ('CG' if degree > 1 else 'DG'), degree-1)
 
+bc_u    = [DirichletBC(V, u_D, lambda x, on_boundary: (x[0] < 1e-6) and on_boundary),]
+bc_dudt = [DirichletBC(V, dudt_D, lambda x, on_boundary: (x[0] < 1e-6) and on_boundary),]
+
+bc_dofs = list(bc_u[0].get_boundary_values().keys())
+
+def compute_bc_values_u():
+    return list(bc_u[0].get_boundary_values().values())
+
+def compute_bc_values_dudt():
+    return list(bc_dudt[0].get_boundary_values().values())
+
 v = TestFunction(V)
 u = TrialFunction(V)
 
-F = ((div(u)-g)*div(v) + curl(u)*curl(v)) * dx
-a, L = lhs(F), rhs(F)
+M = dot(u, v) * dx
+K = inner(grad(u), grad(v)) * dx
 
-bc = [# DirichletBC(V, Constant(( 0.0,-1.0)), lambda x, on_boundary: (x[0] < 1e-6) and on_boundary),
-      # DirichletBC(V, Constant(( 1.0, 0.0)), lambda x, on_boundary: (x[1] < 1e-6) and on_boundary),
-      DirichletBC(V, Constant(( 0.0, 0.0)), lambda x, on_boundary: (x[0] > 1.0-1e-6) and on_boundary)]
+lhs = assemble((1/c_**2)*M + (0.5*theta_*dt_**2)*K)
+rhs_1 = assemble((1/c_**2)*M - (0.5*(1.0-theta_)*dt_**2)*K)
+rhs_2 = assemble(-dt_*K)
 
-u = Function(V, name="electric_field")
+u = Function(V, name="u")
+dudt = Function(V, name="du/dt")
+dudt_k = Function(V, name="du/dt (prev.)")
 
-A = assemble(a)
-b = assemble(L)
+t = 0
 
-for bc_i in bc:
-    bc_i.apply(A, b)
+# This only zeros-out the rows and puts ones on the diagonal (DO ONCE)
+for bc_i in bc_dudt:
+    bc_i.apply(lhs)
 
-solve(A, u.vector(), b)
+try:
 
-div_E = project(div(u), S)
-div_E.rename("div_E", "")
+    i = 0
+    while t < t_final:
 
-curl_E = project(curl(u), S)
-curl_E.rename("curl_E", "")
+        u_D.t = t
+        dudt_D.t = t
 
-fh = plt.figure(1)
-fh.clear()
-plot(u)
-plt.show()
+        dudt_k.vector()[:] = dudt.vector()
 
-File("electric_field.pvd") << u
+        u.vector()[bc_dofs] = compute_bc_values_u()
+        dudt_k.vector()[bc_dofs] = compute_bc_values_dudt()
 
-File("error_div.pvd") << div_E
-File("error_curl.pvd") << curl_E
+        rhs = rhs_1*dudt_k.vector() + rhs_2*u.vector()
 
-print("norm(u):\n", "{0:.6f}".format(norm(u)))
-print("norm(div(u)-g)\n", "{0:.6f}".format(assemble((div_E-g)**2*dx)))
-print("norm(curl(u):\n", "{0:.6f}".format(assemble(curl_E**2*dx)))
+        t += dt
+
+        u_D.t = t
+        dudt_D.t = t
+
+        for bc_i in bc_dudt:
+            bc_i.apply(rhs)
+
+        print("Solve for {0:.3f}".format(t))
+        solve(lhs, dudt.vector(), rhs)
+
+        u.vector()[:] += (dudt_k.vector()*((1.0-theta)*dt) + dudt.vector()*(theta*dt))
+
+        if i % 20 == 0:
+            outfile_u << u
+
+        i += 1
+
+except KeyboardInterrupt:
+    print("\nCought a `KeyboardInterrupt`\n")
+    pass
+
+
+# div_E = project(div(u), S)
+# div_E.rename("div_E", "")
+#
+# curl_E = project(curl(u), S)
+# curl_E.rename("curl_E", "")
+#
+# fh = plt.figure(1)
+# fh.clear()
+# plot(u)
+# plt.show()
+#
+# File("electric_field.pvd") << u
+#
+# File("error_div.pvd") << div_E
+# File("error_curl.pvd") << curl_E
+#
+# print("norm(u):\n", "{0:.6f}".format(norm(u)))
+# print("norm(div(u)-g)\n", "{0:.6f}".format(assemble((div_E-g)**2*dx)))
+# print("norm(curl(u):\n", "{0:.6f}".format(assemble(curl_E**2*dx)))
